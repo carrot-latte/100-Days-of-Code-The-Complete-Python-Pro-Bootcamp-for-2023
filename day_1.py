154b7df7f2939c0232e5e49d4eb71ab59c5eda10
Daniela <122565670+carrot-latte@users.noreply.github.com>
2023-04-24 17:01:34 +0200

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band name generator!')
+name_town = input('Whats is your home town name?\n ')
+name_pet = input('Whats name of your pet?\n ')
+print('Your band name is ' + name_town +" "+ name_pet)
